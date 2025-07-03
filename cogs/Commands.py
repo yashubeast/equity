0d250502979131f662a11e3e5642b02e58c5fd18
yashubeast <shenziouminami@gmail.com>
2025-07-04 00:42:15 +0530

    initial

--- cogs/Commands.py
+++ cogs/Commands.py
@@ -1,151 +1,150 @@
 import os
+import aiohttp
 
 import discord
 from discord import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
 
-from utils.database import BankDB
-
 load_dotenv()
 
 
-@app_commands.guild_only()
-@app_commands.default_permissions(administrator=True)
-class AdminContractGroup(
-    commands.GroupCog, name="admincontract", description="Create/Manage contracts with other users"
-):
-    def __init__(self, bot: commands.Bot):
-        self.bot = bot
+# @app_commands.guild_only()
+# @app_commands.default_permissions(administrator=True)
+# class AdminContractGroup(
+#     commands.GroupCog, name="admincontract", description="Create/Manage contracts with other users"
+# ):
+#     def __init__(self, bot: commands.Bot):
+#         self.bot = bot
+#
+#     @app_commands.command(name="create", description="Create a contract with another user")
+#     @app_commands.describe(user="User you wish to create a contract with")
+#     @app_commands.describe(length="Length of time for contract in days")
+#     @app_commands.describe(amount="Daily amount to be given in the contract")
+#     async def contract_create(
+#         self, itx: discord.Interaction, user: discord.Member, length: int, amount: int
+#     ):
+#         DB = BankDB()
+#         if 1 > length:
+#             await itx.response.send_message(
+#                 "You cant create a contract with a length of less than 1 day.", ephemeral=True
+#             )
+#         DB.contract_add(os.getenv("ServerID"), user.id, amount, length)
+#         await itx.response.send_message(f"Admin contract Created for {user}", ephemeral=True)
+#
+#     @app_commands.command(name="list", description="List of all your current outgoing contracts")
+#     async def contract_list(self, itx: discord.Interaction):
+#         DB = BankDB()
+#         results = DB.list_contracts(os.getenv("ServerID"))
+#         if results == []:
+#             embed = discord.Embed(title=f"{itx.guild.name}'s Contracts", description="No Contracts")
+#             embed.set_thumbnail(
+#                 url=itx.user.avatar.url if itx.user.avatar.url else itx.user.default_avatar
+#             )
+#
+#             embed.set_footer(text="Universium", icon_url=itx.guild.icon)
+#             await itx.response.send_message(embed=embed, ephemeral=True)
+#             return
+#         description = ""
+#         for result in results:
+#             if len(description) > 3980:
+#                 continue
+#             description += "\n"
+#             description += f"{result[0]} - <@{result[2]}> {result[3]} coins * {result[4]} days"
+#         embed = discord.Embed(title=f"{itx.guild.name}'s Contracts", description=description)
+#         embed.set_thumbnail(
+#             url=itx.user.avatar.url if itx.user.avatar.url else itx.user.default_avatar
+#         )
+#
+#         embed.set_footer(text="Universium", icon_url=itx.guild.icon)
+#         await itx.response.send_message(embed=embed, ephemeral=True)
+#
+#     @app_commands.command(name="cancel", description="Cancel an outgoing contract")
+#     @app_commands.describe(contract_id="ID listed on the contract")
+#     async def contract_delete(self, itx: discord.Interaction, contract_id: int):
+#         DB = BankDB()
+#         contracts = DB.list_contracts(os.getenv("ServerID"))
+#
+#         for contract in contracts:
+#             if contract[0] == contract_id:
+#                 DB.delete_contract(contract_id)
+#                 await itx.response.send_message(
+#                     f"The Contract with ID {contract_id} for <@{contract[2]}> has been cancelled",
+#                     ephemeral=True,
+#                 )
+#                 return
+#         await itx.response.send_message(
+#             f"You do not control a contract with ID {contract_id}.", ephemeral=True
+#         )
 
-    @app_commands.command(name="create", description="Create a contract with another user")
-    @app_commands.describe(user="User you wish to create a contract with")
-    @app_commands.describe(length="Length of time for contract in days")
-    @app_commands.describe(amount="Daily amount to be given in the contract")
-    async def contract_create(
-        self, itx: discord.Interaction, user: discord.Member, length: int, amount: int
-    ):
-        DB = BankDB()
-        if 1 > length:
-            await itx.response.send_message(
-                "You cant create a contract with a length of less than 1 day.", ephemeral=True
-            )
-        DB.contract_add(os.getenv("ServerID"), user.id, amount, length)
-        await itx.response.send_message(f"Admin contract Created for {user}", ephemeral=True)
 
-    @app_commands.command(name="list", description="List of all your current outgoing contracts")
-    async def contract_list(self, itx: discord.Interaction):
-        DB = BankDB()
-        results = DB.list_contracts(os.getenv("ServerID"))
-        if results == []:
-            embed = discord.Embed(title=f"{itx.guild.name}'s Contracts", description="No Contracts")
-            embed.set_thumbnail(
-                url=itx.user.avatar.url if itx.user.avatar.url else itx.user.default_avatar
-            )
-
-            embed.set_footer(text="Universium", icon_url=itx.guild.icon)
-            await itx.response.send_message(embed=embed, ephemeral=True)
-            return
-        description = ""
-        for result in results:
-            if len(description) > 3980:
-                continue
-            description += "\n"
-            description += f"{result[0]} - <@{result[2]}> {result[3]} coins * {result[4]} days"
-        embed = discord.Embed(title=f"{itx.guild.name}'s Contracts", description=description)
-        embed.set_thumbnail(
-            url=itx.user.avatar.url if itx.user.avatar.url else itx.user.default_avatar
-        )
-
-        embed.set_footer(text="Universium", icon_url=itx.guild.icon)
-        await itx.response.send_message(embed=embed, ephemeral=True)
-
-    @app_commands.command(name="cancel", description="Cancel an outgoing contract")
-    @app_commands.describe(contract_id="ID listed on the contract")
-    async def contract_delete(self, itx: discord.Interaction, contract_id: int):
-        DB = BankDB()
-        contracts = DB.list_contracts(os.getenv("ServerID"))
-
-        for contract in contracts:
-            if contract[0] == contract_id:
-                DB.delete_contract(contract_id)
-                await itx.response.send_message(
-                    f"The Contract with ID {contract_id} for <@{contract[2]}> has been cancelled",
-                    ephemeral=True,
-                )
-                return
-        await itx.response.send_message(
-            f"You do not control a contract with ID {contract_id}.", ephemeral=True
-        )
-
-
-class ContractGroup(
-    commands.GroupCog, name="contract", description="Create/Manage contracts with other users"
-):
-    def __init__(self, bot: commands.Bot):
-        self.bot = bot
-
-    @app_commands.command(name="create", description="Create a contract with another user")
-    @app_commands.describe(user="User you wish to create a contract with")
-    @app_commands.describe(length="Length of time for contract in days")
-    @app_commands.describe(amount="Daily amount to be given in the contract")
-    async def contract_create(
-        self, itx: discord.Interaction, user: discord.Member, length: int, amount: int
-    ):
-        DB = BankDB()
-        if 1 > length:
-            await itx.response.send_message(
-                "You cant create a contract with a length of less than 1 day.", ephemeral=True
-            )
-        DB.contract_add(itx.user.id, user.id, amount, length)
-        await itx.response.send_message(
-            f"Contract Created between {user} and yourself", ephemeral=True
-        )
-
-    @app_commands.command(name="list", description="List of all your current outgoing contracts")
-    async def contract_list(self, itx: discord.Interaction):
-        DB = BankDB()
-        results = DB.list_contracts(itx.user.id)
-        if results == []:
-            embed = discord.Embed(title=f"{itx.user}'s Contracts", description="No Contracts")
-            embed.set_thumbnail(
-                url=itx.user.avatar.url if itx.user.avatar.url else itx.user.default_avatar
-            )
-
-            embed.set_footer(text="Universium", icon_url=itx.guild.icon)
-            await itx.response.send_message(embed=embed, ephemeral=True)
-            return
-        description = ""
-        for result in results:
-            if len(description) > 3980:
-                continue
-            description += "\n"
-            description += f"{result[0]} - <@{result[2]}> {result[3]} coins * {result[4]} days"
-        embed = discord.Embed(title=f"{itx.user}'s Contracts", description=description)
-        embed.set_thumbnail(
-            url=itx.user.avatar.url if itx.user.avatar.url else itx.user.default_avatar
-        )
-
-        embed.set_footer(text="Universium", icon_url=itx.guild.icon)
-        await itx.response.send_message(embed=embed, ephemeral=True)
-
-    @app_commands.command(name="cancel", description="Cancel an outgoing contract")
-    @app_commands.describe(contract_id="ID listed on the contract")
-    async def contract_delete(self, itx: discord.Interaction, contract_id: int):
-        DB = BankDB()
-        contracts = DB.list_contracts(itx.user.id)
-
-        for contract in contracts:
-            if contract[0] == contract_id:
-                DB.delete_contract(contract_id)
-                await itx.response.send_message(
-                    f"The Contract with ID {contract_id} between yourself and <@{contract[2]}> has been cancelled",
-                    ephemeral=True,
-                )
-                return
-        await itx.response.send_message(
-            f"You do not control a contract with ID {contract_id}.", ephemeral=True
-        )
+# class ContractGroup(
+#     commands.GroupCog, name="contract", description="Create/Manage contracts with other users"
+# ):
+#     def __init__(self, bot: commands.Bot):
+#         self.bot = bot
+#
+#     @app_commands.command(name="create", description="Create a contract with another user")
+#     @app_commands.describe(user="User you wish to create a contract with")
+#     @app_commands.describe(length="Length of time for contract in days")
+#     @app_commands.describe(amount="Daily amount to be given in the contract")
+#     async def contract_create(
+#         self, itx: discord.Interaction, user: discord.Member, length: int, amount: int
+#     ):
+#         DB = BankDB()
+#         if 1 > length:
+#             await itx.response.send_message(
+#                 "You cant create a contract with a length of less than 1 day.", ephemeral=True
+#             )
+#         DB.contract_add(itx.user.id, user.id, amount, length)
+#         await itx.response.send_message(
+#             f"Contract Created between {user} and yourself", ephemeral=True
+#         )
+#
+#     @app_commands.command(name="list", description="List of all your current outgoing contracts")
+#     async def contract_list(self, itx: discord.Interaction):
+#         DB = BankDB()
+#         results = DB.list_contracts(itx.user.id)
+#         if results == []:
+#             embed = discord.Embed(title=f"{itx.user}'s Contracts", description="No Contracts")
+#             embed.set_thumbnail(
+#                 url=itx.user.avatar.url if itx.user.avatar.url else itx.user.default_avatar
+#             )
+#
+#             embed.set_footer(text="Universium", icon_url=itx.guild.icon)
+#             await itx.response.send_message(embed=embed, ephemeral=True)
+#             return
+#         description = ""
+#         for result in results:
+#             if len(description) > 3980:
+#                 continue
+#             description += "\n"
+#             description += f"{result[0]} - <@{result[2]}> {result[3]} coins * {result[4]} days"
+#         embed = discord.Embed(title=f"{itx.user}'s Contracts", description=description)
+#         embed.set_thumbnail(
+#             url=itx.user.avatar.url if itx.user.avatar.url else itx.user.default_avatar
+#         )
+#
+#         embed.set_footer(text="Universium", icon_url=itx.guild.icon)
+#         await itx.response.send_message(embed=embed, ephemeral=True)
+#
+#     @app_commands.command(name="cancel", description="Cancel an outgoing contract")
+#     @app_commands.describe(contract_id="ID listed on the contract")
+#     async def contract_delete(self, itx: discord.Interaction, contract_id: int):
+#         DB = BankDB()
+#         contracts = DB.list_contracts(itx.user.id)
+#
+#         for contract in contracts:
+#             if contract[0] == contract_id:
+#                 DB.delete_contract(contract_id)
+#                 await itx.response.send_message(
+#                     f"The Contract with ID {contract_id} between yourself and <@{contract[2]}> has been cancelled",
+#                     ephemeral=True,
+#                 )
+#                 return
+#         await itx.response.send_message(
+#             f"You do not control a contract with ID {contract_id}.", ephemeral=True
+#         )
 
 
 class CommandsCog(commands.Cog):
@@ -185,12 +184,24 @@ class CommandsCog(commands.Cog):
     #        DB.set_bonus(float_value)
     #        await itx.response.send_message(f"The bonus rate has been set to {bonus}", ephemeral=True)
 
-    @app_commands.command(name="coins", description="Check how many coins you have")
+    @app_commands.command(name="balance", description="Check your Equity balance")
     async def myCoins(self, itx: discord.Interaction):
-        DB = BankDB()
-        balance = DB.get_balance(itx.user.id)
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                "http://localhost:8000/equity/balance",
+                json={
+                    "user_id": str(itx.user.id)
+                }
+            ) as resp:
+                if resp.status == 200:
+                    data = await resp.json()
+                else:
+                    print(f'error: {resp.status}')
+                    return
+
         embed = discord.Embed(
-            title=f"{itx.user}'s Coins", description=f"*You have {balance} coins*"
+            title=f"{itx.user}'s Balance", description=f"*You have {data['result']} Equity*"
         )
 
         embed.set_thumbnail(
@@ -201,20 +212,32 @@ class CommandsCog(commands.Cog):
 
         await itx.response.send_message(embed=embed, ephemeral=True)
 
-    @app_commands.command(name="tradecoins", description="Give another user some of your coins")
+    @app_commands.command(name="pay", description="Give another user Equity")
     async def tradecoins(self, itx: discord.Interaction, user: discord.Member, coins: int):
-        DB = BankDB()
-        selfbalance = DB.get_balance(itx.user.id)
-        if selfbalance > coins:
-            DB.update_balance(itx.user.id, selfbalance - coins)
-            otherbalance = DB.get_balance(user.id)
-            DB.update_balance(user.id, otherbalance + coins)
-            await itx.response.send_message(f"You gave {user} {coins} coins", ephemeral=True)
-            return
-        await itx.response.send_message(f"You dont have {coins} coins to give to {user}.")
+
+        sender_id = str(itx.user.id)
+        receiver_id = str(user.id)
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                "http://localhost:8000/equity/pay",
+                json={
+                    "sender_id": sender_id,
+                    "receiver_id": receiver_id,
+                    "amount": coins
+                }
+            ) as resp:
+                if resp.status == 200:
+                    await itx.response.send_message(f"You gave {user} {coins} Equity", ephemeral=True)
+                    return
+                else:
+                    print(f'error: {resp.status}')
+                    # await itx.response.send_message(f"You dont have {coins} coins to give to {user}.")
+                    await itx.response.send_message(f"Trouble sending Equity to {user}")
+                    return
 
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(CommandsCog(bot))
-    await bot.add_cog(ContractGroup(bot))
-    await bot.add_cog(AdminContractGroup(bot))
+    # await bot.add_cog(ContractGroup(bot))
+    # await bot.add_cog(AdminContractGroup(bot))
--- cogs/Events.py
+++ cogs/Events.py
@@ -1,56 +1,68 @@
 import datetime
 import math
+import aiohttp
 
-from discord.ext import commands, tasks
-
-from utils.database import BankDB
-
+from discord.ext import commands
 
 class EventCog(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.fulfillcontracts.start()
+        # self.fulfillcontracts.start()
 
-    @tasks.loop(time=datetime.time(12, 0))  # Executes at 12:00 PM every day
-    async def fulfillcontracts(self):
-        DB = BankDB()
-        DB.fulfill_contracts()
+    # @tasks.loop(time=datetime.time(12, 0))  # Executes at 12:00 PM every day
+    # async def fulfillcontracts(self):
+    #     DB = BankDB()
+    #     DB.fulfill_contracts()
 
-    @fulfillcontracts.before_loop
-    async def before_fulfillcontracts(self):
-        # Wait until the bot is ready before starting the loop
-        await self.bot.wait_until_ready()
+    # @fulfillcontracts.before_loop
+    # async def before_fulfillcontracts(self):
+    #     # Wait until the bot is ready before starting the loop
+    #     await self.bot.wait_until_ready()
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        user = message.author
+        user = (message.author)
         if message.author.bot:
             return
-        current_datetime = datetime.datetime.utcnow()
-        DB = BankDB()
-        messages = DB.get_messages(user.id)
-        lastmessage = DB.get_last_message(user.id)
-        bonus = DB.bonus_rate()
-        bonus = float(bonus)
-        lastmessage = datetime.datetime.strptime(lastmessage, "%Y-%m-%d %H:%M:%S.%f")
-        time_difference = (current_datetime - lastmessage).total_seconds()
-        time_value = time_difference * 0.15
-        if time_value > 1:
-            overflowValue = 1.2 * (math.log(1 + (time_difference - 7 / 60)) / math.log(61))
-            time_value = 1 + overflowValue
-        total = len(message.content) * (1 + (bonus * messages)) * time_value
-        tax = DB.tax_rate()
-        Mastercoins = int(total) * float(tax)
-        userCoins = int(total - Mastercoins)
-        Mastercoins = total - userCoins + Mastercoins
-        DB.event_add(user.id, userCoins, Mastercoins, message)
+
+        user_id = str(user.id)
+        message_id = str(message.id)
+        message_length = len(message.content)
+        timestamp = str(datetime.datetime.utcnow().timestamp())
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                "http://localhost:8000/equity/eval",
+                json={
+                    "user_id": user_id,
+                    "message_id": message_id,
+                    "message_length": message_length,
+                    "timestamp": timestamp,
+                }
+            ) as resp:
+                if resp.status == 200:
+                    data = await resp.json()
+                    print(f'{user_id}: {message_id} -> len: {message_length}, time: {timestamp}')
+                else:
+                    print(f"api error: {resp.status}")
 
     @commands.Cog.listener()
     async def on_raw_message_delete(self, payload):
-        DB = BankDB()
-        DB.delete_message(payload.message_id)
+        
+        message_id = str(payload.message_id)
 
+        async with aiohttp.ClientSession() as session:
+            async with session.delete(
+                "http://localhost:8000/equity/del",
+                json={
+                    "message_id": message_id,
+                }
+            ) as resp:
+                if resp.status == 200:
+                    print(f'msg deleted: {message_id}')
+                else:
+                    print(f"api error: {resp.status}")
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(EventCog(bot))
