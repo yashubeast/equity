e2dfaea24e04e13d1ca98d71285fe00d0bd3f128
yashubeast <shenziouminami@gmail.com>
2025-08-27 21:33:59 +0530

    Revert "say cmd" oopsies
    This reverts commit 3dcd25b0880ec594e59e6829be081e67309281cb.

--- Makefile
+++
@@ -1,8 +0,0 @@
-build:
-	docker build -t equity . --no-cache
-
-prod:
-	docker build -t yashubeast/nerv:equity . --no-cache
-
-push:
-	docker push yashubeast/nerv:equity
--- utils/error.py
+++ utils/error.py
@@ -1,5 +1,4 @@
 from discord import Interaction
-from discord.errors import NotFound
 from discord.app_commands import AppCommandError
 from discord.app_commands.errors import CheckFailure
 from utils.logger import log
@@ -10,9 +9,5 @@ async def app_command_error_handler(itx: Interaction, error: AppCommandError):
     if isinstance(error, NotAdmin):
         await itx.response.send_message("> Equity Admins only command", ephemeral=True)
         log.warning(f"non-admin user {itx.user} tried to use /{itx.command.qualified_name}")
-    
-    if isinstance(error, NotFound) and "Unknown interaction" in str(error):
-        log.warning(f"expired interaction command '{itx.command.name}' from user {itx.user.id} expired before response")
-        return
-
-    raise error
+    else:
+        raise error
